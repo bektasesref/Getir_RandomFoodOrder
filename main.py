4ee59cb8b771ed36b80734854e07c6abc902e626
Studio Billion <esref@studiobillion.com>
2024-01-03 13:15:16 +0300

    feat: initial commit with login w/ caching, random favorite restaurant pick

---
+++ main.py
@@ -0,0 +1,115 @@
+#region PIP Setup
+#chromedriver-py
+#selenium
+#endregion
+
+#region Imports
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from chromedriver_py import binary_path
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import random
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+#endregion
+
+#region Global Parameters
+wait = WebDriverWait(None,0)
+driver = webdriver.Chrome()
+#endregion
+
+#region Custom Input Parameters
+phoneNumberToLogin = "531[replace_with_your_number]"
+#endregion
+
+#region Selenium XPath, CSS Selector, TAG & Name
+loginPage_phoneNumberInputArea_Name = "gsm"
+loginPage_confirmSMSCode_CSS = "body > div  > div  > div > div > div> form > div > div > div > div > input"
+
+favoritesPage_container_XPath = "div > main > div > div > section > div:last-child > div:last-child"
+favoritesPage_container_item_CSS = "div > main > div > div > section > div:last-child > div:last-child > article:nth-child(INDEX) > div > div > div > div > a"
+favoritesPage_container_item_isOpen_CSS = "div > main > div > div > section > div:last-child > div:last-child > article:nth-child(INDEX) > div > div > div:nth-child(2) > div:nth-child(2) > div"
+#endregion
+
+#region Flow
+def login():
+    if driver.current_url == "https://getir.com/yemek/":
+        print("Input Area Waiting")
+        wait.until(EC.visibility_of_element_located((By.NAME, loginPage_phoneNumberInputArea_Name))).send_keys(phoneNumberToLogin+Keys.ENTER)
+        print("Phone Number Entered")
+        print("Waiting for SMS Code to Enter Manually")
+        print("Waiting for Main Page to Load")
+        print(driver.find_element(By.CSS_SELECTOR, loginPage_confirmSMSCode_CSS).get_attribute("value"))
+        while(len(driver.find_element(By.CSS_SELECTOR, loginPage_confirmSMSCode_CSS).get_attribute("value")) != 4):
+            continue
+        driver.find_element(By.CSS_SELECTOR, loginPage_confirmSMSCode_CSS).send_keys(Keys.ENTER)
+
+        wait.until(EC.url_to_be("https://getir.com/yemek/restoranlar/"))
+    else:
+        print("Already Logged In")
+
+    driver.get("https://getir.com/hesap/favori-restoranlarim/")
+
+def get_favorite_restaurant_count():
+    #TODO: pagination must be considerated, not doing right now
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, favoritesPage_container_XPath)))
+    favorites_section = driver.find_element(By.CSS_SELECTOR, favoritesPage_container_XPath)
+    return favorites_section.get_attribute("childElementCount")
+
+def choose_random_restaurant(favorite_count):
+    if int(favorite_count) > 0:
+        randomly_chosen_restaurant = str(random.randint(1, int(favorite_count)))
+        print("Randomly Chosen Restaurant Index: " + randomly_chosen_restaurant)
+        is_open = driver.find_element(By.CSS_SELECTOR, favoritesPage_container_item_isOpen_CSS.replace("INDEX", randomly_chosen_restaurant))
+        is_open = bool(is_open.get_attribute("textContent") == "")
+        print("Is Open?: " + str(is_open))
+
+        if is_open:
+            restaurant_click = driver.find_element(By.CSS_SELECTOR, favoritesPage_container_item_CSS.replace("INDEX", randomly_chosen_restaurant))
+            random_link = restaurant_click.get_attribute("href")
+            print("Random Link: " + random_link)
+            driver.execute_script('''window.open("''' + random_link + '''","_blank");''')
+            driver.switch_to.window(driver.window_handles[-1])
+        else:
+            print("Selected restaurant is not open. Choosing another one.")
+            choose_random_restaurant(favorite_count)
+    else:
+        print("No favorite restaurant found. Quitting...")
+
+#endregion
+
+#region Initializers
+def main():
+    global driver
+    driver = initialize_driver()
+
+    driver.get("https://getir.com/yemek/restoranlar/")
+    driver.implicitly_wait(150000)
+
+    global wait
+    wait = WebDriverWait(driver, 150)
+
+    login()
+
+    favorite_count = get_favorite_restaurant_count()
+    print("Favorite Restaurant Count: " + favorite_count)
+
+    choose_random_restaurant(favorite_count)
+    print("Random Selected Restaurant: " + driver.title)
+
+    #TODO: select food by presets for specific restaurant. For example, in Unkapanı Pilavıcısı, Select Tavuklu Pilav to the shopping cart with 1 portion
+    #TODO: After selections has been done, go through shoppnig cart section and make an order
+
+    driver.quit()
+
+def initialize_driver():
+    svc = webdriver.ChromeService(executable_path=binary_path)
+    chrome_options = Options()
+    chrome_options.add_argument("user-data-dir=browser_cache")
+    return webdriver.Chrome(service=svc, options=chrome_options)
+#endregion
+
+if __name__ == "__main__":
+    main()
